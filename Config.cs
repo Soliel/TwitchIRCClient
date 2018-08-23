2bea9a7460e67a29576c957a9371ae25cc1780c8
realityquintupled <40674794+realityquintupled@users.noreply.github.com>
2018-08-23 02:44:34 -0400

    Basic Boy Version™

---
+++ Config.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace TwitchIRCClient
+{
+    public class Config
+    {
+        public string username = "username here";
+        public string oAuthToken = "OAuth password here";
+        public string channelName = "channel to connect to";
+        public static Config LoadConfig(string configPath)
+        {
+            if(!File.Exists(configPath))
+            {
+                File.Create(configPath).Dispose();
+                Config config = new Config();
+                config.SaveConfig(configPath);
+            }
+            return JsonUtility.FromJson<Config>(File.ReadAllText(configPath));
+        }
+        public void SaveConfig(string configPath)
+        {
+            File.WriteAllText(configPath, JsonUtility.ToJson(this, true));
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,48 @@
+﻿using IllusionPlugin;
+using System;
+using System.IO;
+using UnityEngine;
+
+namespace TwitchIRCClient
+{
+    public class Plugin : IPlugin
+    {
+        public string Name => "TwitchIRCClient";
+        public string Version => "v0.0.1";
+        private string configPath = Path.Combine(Environment.CurrentDirectory, "TwitchClientConfig.json");
+        public static TwitchClient twitchClient;
+        public void OnApplicationStart()
+        {
+            Config config = Config.LoadConfig(configPath);
+            twitchClient = new GameObject("TwitchClient").AddComponent<TwitchClient>();
+            twitchClient.SetCredentials(config.oAuthToken, config.username, config.channelName);
+            UnityEngine.Object.DontDestroyOnLoad(GameObject.Find("TwitchClient"));
+        }
+
+        public void OnApplicationQuit()
+        {
+        }
+
+        public void OnLevelWasLoaded(int level)
+        {
+
+        }
+
+        public void OnLevelWasInitialized(int level)
+        {
+        }
+
+        public void OnUpdate()
+        {
+        }
+
+        public void OnFixedUpdate()
+        {
+        }
+
+        public static void Log(string message)
+        {
+            Console.WriteLine("[TwitchIRCClient] " + message);
+        }
+    }
+}
